51aac1169a1af0458f413a9535be14c1473acfa8
kr5hn4 <cod3ncoff33@gmail.com>
2024-11-18 04:32:23 +0530

    Add nvim configurations

---
+++ nvim/.config/nvim/lua/kickstart/plugins/mini.lua
@@ -0,0 +1,12 @@
+return {
+  { -- Collection of various small independent plugins/modules
+    'echasnovski/mini.nvim',
+    config = function()
+      -- Better Around/Inside textobjects
+      require('mini.ai').setup { n_lines = 500 }
+
+      -- Add/delete/replace surroundings (brackets, quotes, etc.)
+      require('mini.surround').setup()
+    end,
+  },
+}
