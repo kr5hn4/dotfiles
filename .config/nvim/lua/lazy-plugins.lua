e134c2a01d64dc4100d74229972f24c0f23cdd68
kr5hn4 <cod3ncoff33@gmail.com>
2025-10-09 18:11:41 +0530

    feat: Update lazy-lock.json and lazy-plugins file

--- .config/nvim/lua/lazy-plugins.lua
+++ .config/nvim/lua/lazy-plugins.lua
@@ -3,33 +3,25 @@ require("lazy").setup({
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	-- 'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
-	require("kickstart/plugins/git"),
-
-	require("kickstart/plugins/telescope"),
-
-	require("kickstart/plugins/lsp"),
-
-	require("kickstart/plugins/format"),
-
-	require("kickstart/plugins/autocomplete"),
-
-	require("kickstart/plugins/theme"),
-
-	require("kickstart/plugins/comments"),
-
-	require("kickstart/plugins/mini"),
-
-	require("kickstart/plugins/treesitter"),
-
-	require("kickstart/plugins/nvim-tree"),
-
-	require("kickstart/plugins/tmux-navigator"),
-
-	require("kickstart/plugins/llm"),
-
-	require("kickstart/plugins/oil"),
-
-	require("kickstart/plugins/debug"),
+	require("config/autocomplete"),
+	require("config/autopairs"),
+	require("config/code-companion"),
+	require("config/comments"),
+	require("config/debug"),
+	require("config/format"),
+	require("config/git"),
+	require("config/indent_line"),
+	require("config/lint"),
+	require("config/lsp"),
+	require("config/lspkind"),
+	require("config/mini"),
+	require("config/nvim-tree"),
+	require("config/oil"),
+	-- require("config/snippets"),
+	require("config/telescope"),
+	require("config/theme"),
+	require("config/tmux-navigator"),
+	require("config/treesitter"),
 }, {
 	ui = {
 		-- If you are using a Nerd Font: set icons to an empty table which will use the
