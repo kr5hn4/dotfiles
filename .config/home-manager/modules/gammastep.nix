32f4c18e2c23676b2f3d97fc652fa253c7d430f1
kr5hn4 <cod3ncoff33@gmail.com>
2026-01-07 02:28:50 +0530

    Fix gammastep config

--- .config/home-manager/modules/gammastep.nix
+++ .config/home-manager/modules/gammastep.nix
@@ -1,18 +1,21 @@
-{...}: {
+{lib, ...}: {
   services.gammastep = {
     enable = true;
-
+    provider = "manual";
     temperature = {
-      day = 6500;
-      night = 3800;
+      day = 6500; # Normal during day
+      night = 3800; # Red/warm at night
     };
-
-    # Fixed schedule (no sunrise/sunset math)
-    dawnTime = "07:00";
-    duskTime = "20:00";
-
-    # Required even when using fixed times
+    dawnTime = "07:00-07:30"; # Start going back to normal at 7am (30 min fade)
+    duskTime = "20:00-20:30"; # Start going red at 8pm (30 min fade)
     latitude = 0.0;
     longitude = 0.0;
+  };
+  systemd.user.services.gammastep = {
+    Unit = {
+      After = lib.mkForce [];
+      PartOf = lib.mkForce [];
+    };
+    Install.WantedBy = lib.mkForce ["default.target"];
   };
 }
